3db46900661fe2428899b7ee18e9d4622d8ffc64
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-02-07 13:19:13 +0530

    Add junk test. Use junit4

---
+++ src/test/java/com/turbulence/ClusterSpaceTest.java
@@ -0,0 +1,19 @@
+package com.turbulence;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClusterSpaceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	    System.out.println("Boom");
+	}
+
+	@Test
+	public void testLink() {
+	}
+
+}
