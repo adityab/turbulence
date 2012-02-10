f000d2fcfbe57fe07104aebda5679750cc95f300
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-02-10 10:13:30 +0530

    Added config parser

---
+++ src/main/java/com/turbulence/util/ConfigParserException.java
@@ -0,0 +1,24 @@
+package com.turbulence.util;
+
+public class ConfigParserException extends Exception {
+
+    public ConfigParserException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ConfigParserException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ConfigParserException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ConfigParserException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
