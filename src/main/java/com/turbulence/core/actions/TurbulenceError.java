75531a422595fc969e6d3f42498ce5653b9de68a
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-02-14 10:14:04 +0530

    Added error enumeration

---
+++ src/main/java/com/turbulence/core/actions/TurbulenceError.java
@@ -0,0 +1,9 @@
+package com.turbulence.core.actions;
+
+public enum TurbulenceError {
+    SCHEMA_IS_ALREADY_REGISTERED,
+    SCHEMA_REGISTRATION_FAILED,
+    ONTOLOGY_CREATION_FAILED,
+    THREAD_INTERRUPTED,
+    UNKNOWN_ERROR // always keep this LAST in the set
+}
