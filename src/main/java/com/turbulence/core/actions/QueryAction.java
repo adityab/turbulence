02345ec02aacdf5963969d1223c1bb094dbffa79
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-03-17 10:55:30 +0530

    Get rid of unused Visitor

--- src/main/java/com/turbulence/core/actions/QueryAction.java
+++ src/main/java/com/turbulence/core/actions/QueryAction.java
@@ -32,121 +32,6 @@ import com.hp.hpl.jena.query.QueryFactory;
 
 import com.turbulence.core.ClusterSpaceJenaGraph;
 
-class Visitor extends ElementVisitorBase {
-    @Override
-    public void visit(ElementAssign el) {
-        // TODO Auto-generated method stub
-        System.err.println("assign");
-    }
-
-    @Override
-    public void visit(ElementBind el) {
-        // TODO Auto-generated method stub
-        System.err.println("bind");
-    }
-
-    @Override
-    public void visit(ElementDataset el) {
-        // TODO Auto-generated method stub
-        System.err.println("dataset");
-    }
-
-    @Override
-    public void visit(ElementExists el) {
-        // TODO Auto-generated method stub
-        System.err.println("exists");
-    }
-
-    @Override
-    public void visit(ElementFetch el) {
-        // TODO Auto-generated method stub
-        System.err.println("fetch");
-    }
-
-    @Override
-    public void visit(ElementFilter el) {
-        // TODO Auto-generated method stub
-        System.err.println("filter");
-        Expr exp = el.getExpr();
-    }
-
-    @Override
-    public void visit(ElementMinus el) {
-        // TODO Auto-generated method stub
-        System.err.println("minus");
-    }
-
-    @Override
-    public void visit(ElementNamedGraph el) {
-        // TODO Auto-generated method stub
-        System.err.println("named");
-    }
-
-    @Override
-    public void visit(ElementNotExists el) {
-        // TODO Auto-generated method stub
-        System.err.println("notexists");
-    }
-
-    @Override
-    public void visit(ElementOptional el) {
-        // TODO Auto-generated method stub
-        System.err.println("optional");
-    }
-
-    @Override
-    public void visit(ElementPathBlock el) {
-        // TODO Auto-generated method stub
-        System.err.println("pathblock");
-        Iterator<TriplePath> trips = el.patternElts();
-        while (trips.hasNext()) {
-            Triple t = trips.next().asTriple();
-            //System.err.println(t.getSubject().getURI() + " " + t.getPredicate().getURI() + " " + t.getObject().getName());
-            Node obj = t.getObject();
-            if (obj.isURI()) {
-                System.err.println("Object " + obj.getURI());
-            }
-            else if (obj.isVariable()) {
-                System.err.println("Object " + obj.getName());
-            }
-            else if (obj.isLiteral()) {
-                System.err.println("Object " + obj.getLiteralValue());
-            }
-        }
-    }
-
-    @Override
-    public void visit(ElementService el) {
-        // TODO Auto-generated method stub
-        System.err.println("service");
-    }
-
-    @Override
-    public void visit(ElementSubQuery el) {
-        // TODO Auto-generated method stub
-        System.err.println("subquery");
-    }
-
-    @Override
-    public void visit(ElementUnion el) {
-        // TODO Auto-generated method stub
-        System.err.println("Union");
-    }
-
-    public void visit(ElementGroup g) {
-        System.err.println("Group");
-        for (Element el : g.getElements())
-            el.visit(new Visitor());
-    }
-
-    public void visit(ElementTriplesBlock el) {
-        System.err.println("Trip block");
-        Iterator<Triple> trips = el.patternElts();
-        while (trips.hasNext())
-            System.err.println(trips.next().getSubject().getName());
-    }
-}
-
 public class QueryAction implements Action {
     private static final Log logger = LogFactory.getLog(QueryAction.class);
 
@@ -169,7 +54,6 @@ public class QueryAction implements Action {
         return new StreamingOutput() {
             public void write(OutputStream out) throws IOException, WebApplicationException {
                 while (resultSet.hasNext()) {
-                logger.warn("Called");
                     QuerySolution row = resultSet.next();
                     for (String s : resultSet.getResultVars())
                         out.write(row.get(s).toString().getBytes());
