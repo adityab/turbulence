02357bb9db5fed0441a52f3a4f7a0e6401af172e
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-03-29 17:00:06 +0530

    QueryAction uses AllColumnsIterator and cleaned up some junk

--- src/main/java/com/turbulence/core/actions/QueryAction.java
+++ src/main/java/com/turbulence/core/actions/QueryAction.java
@@ -11,8 +11,6 @@ import javax.ws.rs.core.StreamingOutput;
 
 import javax.ws.rs.WebApplicationException;
 
-import org.apache.commons.lang.UnhandledException;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -28,40 +26,16 @@ import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryFactory;
 
 import com.turbulence.core.ClusterSpaceJenaGraph;
+import com.turbulence.core.TurbulenceDriver;
 
-import me.prettyprint.cassandra.model.ConfigurableConsistencyLevel;
+import com.turbulence.util.AllColumnsIterator;
 
 import me.prettyprint.cassandra.serializers.StringSerializer;
-import me.prettyprint.cassandra.serializers.UUIDSerializer;
 
-import me.prettyprint.cassandra.service.ColumnSliceIterator;
-
-import me.prettyprint.cassandra.service.template.ColumnFamilyTemplate;
-import me.prettyprint.cassandra.service.template.ThriftColumnFamilyTemplate;
-
-import me.prettyprint.cassandra.service.ThriftKsDef;
-
-import me.prettyprint.hector.api.beans.ColumnSlice;
 import me.prettyprint.hector.api.beans.HColumn;
-import me.prettyprint.hector.api.beans.OrderedRows;
-import me.prettyprint.hector.api.beans.Row;
-import me.prettyprint.hector.api.beans.Rows;
-
-import me.prettyprint.hector.api.Cluster;
-
-import me.prettyprint.hector.api.ddl.ColumnFamilyDefinition;
-import me.prettyprint.hector.api.ddl.ComparatorType;
-import me.prettyprint.hector.api.ddl.KeyspaceDefinition;
-
-import me.prettyprint.hector.api.exceptions.HectorException;
 
 import me.prettyprint.hector.api.factory.HFactory;
 
-import me.prettyprint.hector.api.HConsistencyLevel;
-import me.prettyprint.hector.api.Keyspace;
-
-import me.prettyprint.hector.api.query.MultigetSliceQuery;
-import me.prettyprint.hector.api.query.RangeSlicesQuery;
 import me.prettyprint.hector.api.query.SliceQuery;
 
 public class QueryAction implements Action {
@@ -94,25 +68,6 @@ public class QueryAction implements Action {
         QueryExecution exec = QueryExecutionFactory.create(q, model);
         final ResultSet resultSet = exec.execSelect();
 
-        ColumnFamilyTemplate<String, UUID> template;
-        Cluster myCluster = HFactory.getOrCreateCluster("turbulence", "localhost:9160");
-        ColumnFamilyDefinition cfDef = HFactory.createColumnFamilyDefinition("Turbulence",
-                "Main",
-                ComparatorType.UUIDTYPE);
-        cfDef.setKeyValidationClass("UTF8Type");
-        KeyspaceDefinition kspd = myCluster.describeKeyspace("Turbulence");
-        if (kspd == null) {
-            KeyspaceDefinition newKeyspace = HFactory.createKeyspaceDefinition("Turbulence", ThriftKsDef.DEF_STRATEGY_CLASS, 1 /*TODO replication factor*/, Arrays.asList(cfDef));
-            myCluster.addKeyspace(newKeyspace, true);
-        }
-        // FIXME: in a cluster we don't want this
-        ConfigurableConsistencyLevel lvl = new ConfigurableConsistencyLevel();
-        lvl.setDefaultReadConsistencyLevel(HConsistencyLevel.ONE);
-        lvl.setDefaultWriteConsistencyLevel(HConsistencyLevel.ONE);
-        final Keyspace ksp = HFactory.createKeyspace("Turbulence", myCluster, lvl);
-
-        template = new ThriftColumnFamilyTemplate<String, UUID>(ksp, "Main", StringSerializer.get(), UUIDSerializer.get());
-
         return new StreamingOutput() {
             public void write(OutputStream out) throws IOException, WebApplicationException {
                 Set<String> rowKeys = new HashSet<String>();
@@ -124,40 +79,15 @@ public class QueryAction implements Action {
                         rowKeys.add(row.getResource(var).getURI());
                     }
                 }
-                for (String r : rowKeys) {
-                    logger.warn("Key " + r);
-                }
+                logger.warn("RESULT set size " + rowKeys.size());
 
                 for (String rowKey : rowKeys) {
-                    SliceQuery<String, UUID, String> query = HFactory.createSliceQuery(ksp, StringSerializer.get(), UUIDSerializer.get(), StringSerializer.get());
+                    SliceQuery<String, String, String> query = HFactory.createSliceQuery(TurbulenceDriver.getKeyspace(), StringSerializer.get(), StringSerializer.get(), StringSerializer.get());
                     query.setKey(rowKey);
-                    query.setColumnFamily("Main");
-                    UUID start = null;
-                    int count = 100;
-                    try {
-                        while (true) {
-                            query.setRange(start, null, false, count);
-                            ColumnSlice<UUID, String> slice = query.execute().get();
-                            List<HColumn<UUID, String>> columns = slice.getColumns();
-                            int origSize = columns.size();
-                            // next start is last one of this
-                            // but we also drop it so that we don't have to
-                            // serve duplicates
-                            // but if number of columns we got is LESS than
-                            // count already, then return all columns
-                            if (origSize >= count)
-                                start = columns.remove(columns.size()-1).getName();
-
-                            for (HColumn<UUID, String> col : columns)
-                                out.write(col.getValue().getBytes());
-
-                            if (origSize < count)
-                                break;
-                        }
-                    } catch (HectorException e) {
-                        logger.warn("Exception processing " + rowKey + " start:" + start + " count:" + count + " -- " + e);
-                        //throw new UnhandledException(e);
-                    }
+                    query.setColumnFamily("ConceptsInstancesData");
+                    Iterator<HColumn<String, String>> it = new AllColumnsIterator<String, String>(query);
+                    while (it.hasNext())
+                        out.write(it.next().getValue().getBytes());
                 }
             }
         };
